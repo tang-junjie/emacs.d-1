5f04030ff970b6bd366bf75c2486e107e9f3c04d
Adam Schwartz <adam@anschwa.com>
2017-05-23 20:23:10 -0500

    maintenance and new languages

--- custom.el
+++ custom.el
@@ -10,15 +10,18 @@
  '(custom-safe-themes
    (quote
     ("2b8430c61f131835f3982be6ada0b5e143009d2e20b421236da77599d1a9cc91" "d44f2ea3769a8d1e7aff493017e0478ab8d588ae522203cbe6bf5f489eeee131" "c03e3b4dc4e334b0dae8b5fef4dc7779dc1585715242089f8ae957c8ba52e7b2" "f2f136616de73efb79a351ab1148d11904819eea0ac4006c29f0e80be303430a" "aeee3059c01facb91b0242093836f90c289aca02cdae75155b0a65802ade2896" "4a44b66f173c853eb958d7cd1e768f34842c6f6eb1219bef6497163d4a1c3a16" "4af8fdcdfa856ccc9189d650e4cff7080dc9987222595a5b7ca5aeac5116547b" "17af5e59d95c0d5e281c97b224f33ecb050b0b1fceb90757d0524a7413a3bcb0" "92e17321f3da624d2eaff713193e3c87e5d0369ced92e8f2ff3f18285550809e" "aa78b7400604222b7a4e234d97bc6481f0776cb1a5b164267cfeb5ef3c4e6b1a" "995e90460de45d952ba075f70536e09497bf5886f47d09267eb4febb2944b98e" "2100d39a39d88125f985dce7701e8da21dd5be3d42e8448d9f2226e9435a4f10" "26e580d9439e556c01af6c064a96fe14c1b7711959d8bb7207734a487a6744af" "eabc5ea3ddd5882f4b5adb6fe1dd6b93a803b1f636d3500f1aea60ecd05c39c3" "60caed2f3f257a8f93344a95407c1f4f103483c274352484bb647b6a7f2b8c7e" "12cfda16558fb0819a9797667a68db8b369a9bccbe701103f44c10b6de1560cc" "010fe7f3245d80d54f4bd0443fdcda395c6a42f60fab623bcad2e00e942aa490" "461a9116ecd6685df6d93529cebeb4d6230a1c27194f2d27e90c2a43809b4875" "873e2cff53842ed1cc1eb3259b312629f0651697e4599c64431421ca1960e7f5" "f27bccd1db0a4f589c980e088fa954902f2e9bb1d1704a648dc3adead4d73970" "364bea0419a2ba563ced224922c9ae1f0a59e71b19ff4f99e9d53905f8d60be7" "b9c008ef53ec1460f75a299962f0889a05247dd7782f5bf99ea9faf73c66eb93" "b2983347254cb316824e8fae22c88c5e6b87fdb8f6a6cae923d4d1f85e6636df" "5fda99c5fcba167cbe5a19de7d82f151bafb41771b454de0b53ab01a980c3733" "3fdbad52766a8f2d8101fb1740aff4e330220612a9d48d8a2fd6ef777e8c90e9" "0900cb5a83b5d7d47e8becf615fac8ce8d3e50a2383e2854e17b1993fc5397c2" "46739dd7e61b0cf8980fc0dc9b9df576963423ee7aec7839db89dc2eaf96bb47" "744c4448e534a0da809575a3e44e7f553636cb919fa61aaff145b2d3663e0514" default)))
+ '(electric-indent-mode nil)
+ '(electric-pair-mode t)
  '(elpy-modules
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-sane-defaults)))
  '(exec-path-from-shell-check-startup-files nil)
  '(package-selected-packages
    (quote
-    (racket-mode exec-path-from-shell expand-region multiple-cursors writegood-mode rainbow-mode smartparens move-text ace-window undo-tree smex writeroom-mode magit geiser rust-mode nim-mode haskell-mode emmet-mode js2-mode web-mode company-jedi use-package)))
+    (slime landmark markdown-mode yaml-mode zoom-frm go-mode paredit racket-mode exec-path-from-shell expand-region multiple-cursors writegood-mode rainbow-mode smartparens move-text ace-window undo-tree smex writeroom-mode magit geiser rust-mode nim-mode haskell-mode emmet-mode js2-mode web-mode company-jedi use-package)))
  '(python-shell-exec-path (quote ("/usr/local/bin/python3")))
- '(python-shell-interpreter "/usr/local/bin/python3"))
+ '(python-shell-interpreter "/usr/local/bin/python3")
+ '(safe-local-variable-values (quote ((Base . 10) (Syntax . Common-Lisp)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
